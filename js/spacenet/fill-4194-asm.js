e5b46c893655744f4128be8c3edc5db7a91b0226
notisastranov <notisastranov@gmail.com>
2026-09-07 22:30:45 +0300

    SpaceNet 4194: assembler eval parts

---
+++ js/spacenet/fill-4194-asm.js
@@ -0,0 +1,5 @@
+(function(){
+  var p=window.__SN4194P; if(!p||!p[0]||!p[1]||!p[2]) return;
+  if(window.__SN_FILL_4194_ASM) return; window.__SN_FILL_4194_ASM=true;
+  try{ (0,eval)(p[0]+p[1]+p[2]); }catch(e){ console&&console.error&&console.error("SN4194",e); }
+})();
